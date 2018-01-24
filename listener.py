b85038c773cc3adb67d367a5fe29f438c101a3d7
Andrei <cunbmev3@gmail.com>
2018-01-24 19:14:42 +0200

    Make robot speak

--- listener.py
+++ listener.py
@@ -1,18 +1,25 @@
+from utils import Utils
 import speech_recognition as sr
 
-# obtain audio from the microphone
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Please wait. Calibrating microphone...")
-    # listen for 5 seconds and create the ambient noise energy level
-    r.adjust_for_ambient_noise(source, duration=5)
-    print("Say something!")
-    audio = r.listen(source)
 
-    # recognize speech using Sphinx
-try:
-    print("Sphinx thinks you said '" + r.recognize_bing(audio, "ad12250025974cf2835a3dffe61ecf99") + "'")
-except sr.UnknownValueError:
-    print("Sphinx could not understand audio")
-except sr.RequestError as e:
-    print("Sphinx error; {0}".format(e))
+class listener:
+    def __init__(self, name, noiseControl= 3):
+        self.name = name
+        self.noiseControl = noiseControl
+        self.r = sr.Recognizer()
+
+    def listen(self):
+        with sr.Microphone() as source:
+            print("Please wait. Calibrating microphone...")
+            # listen for 3 seconds and create the ambient noise energy level
+            self.r.adjust_for_ambient_noise(source, duration=self.noiseControl)
+            audio = self.r.listen(source)
+
+        try:
+            print("'" + self.r.recognize_bing(audio, Utils.bingApiKey) + "'")
+        except sr.UnknownValueError:
+            print(Utils.roboName, "could not understand audio")
+        except sr.RequestError as e:
+            print(Utils.roboName, "error; {0}".format(e))
+
+
--- start.py
+++ start.py
@@ -1,23 +1,4 @@
-# from connector import ev3
-#
-# m = ev3.LargeMotor()
-# m.run_timed(time_sp=3000, speed_sp=1000)
-from utils import Utils
-import speech_recognition as sr
+from connector import ev3
 
-# obtain audio from the microphone
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Please wait. Calibrating microphone...")
-    # listen for 3 seconds and create the ambient noise energy level
-    r.adjust_for_ambient_noise(source, duration=3)
-    print("Say something!")
-    audio = r.listen(source)
-
-    # recognize speech using Sphinx
-try:
-    print("'" + r.recognize_bing(audio, Utils.bingApiKey) + "'")
-except sr.UnknownValueError:
-    print("Sphinx could not understand audio")
-except sr.RequestError as e:
-    print("Sphinx error; {0}".format(e))
+m = ev3.LargeMotor()
+m.run_timed(time_sp=3000, speed_sp=1000)
