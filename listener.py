96f3270217c4bd2a6ddf6b6bdda6b859e2d343b1
Andrei <cunbmev3@gmail.com>
2018-01-24 19:34:35 +0200

    Make robot speak

--- listener.py
+++ listener.py
@@ -3,23 +3,19 @@ import speech_recognition as sr
 
 
 class listener:
-    def __init__(self, name, noiseControl= 3):
-        self.name = name
-        self.noiseControl = noiseControl
-        self.r = sr.Recognizer()
 
-    def listen(self):
+    def listen(noiseControl=0):
+        r = sr.Recognizer()
         with sr.Microphone() as source:
-            print("Please wait. Calibrating microphone...")
-            # listen for 3 seconds and create the ambient noise energy level
-            self.r.adjust_for_ambient_noise(source, duration=self.noiseControl)
-            audio = self.r.listen(source)
-
+            if noiseControl != 0:
+                print("Please wait. Calibrating microphone...")
+                # listen for 3 seconds and create the ambient noise energy level
+                r.adjust_for_ambient_noise(source, duration=noiseControl)
+            print("You can now talk..")
+            audio = r.listen(source)
         try:
-            print("'" + self.r.recognize_bing(audio, Utils.bingApiKey) + "'")
+            return r.recognize_bing(audio, Utils.bingApiKey)
         except sr.UnknownValueError:
             print(Utils.roboName, "could not understand audio")
         except sr.RequestError as e:
             print(Utils.roboName, "error; {0}".format(e))
-
-
--- start.py
+++ start.py
@@ -1,4 +1,6 @@
-from connector import ev3
-
-m = ev3.LargeMotor()
-m.run_timed(time_sp=3000, speed_sp=1000)
+# from connector import ev3
+from listener import listener
+#
+# m = ev3.LargeMotor()
+# m.run_timed(time_sp=3000, speed_sp=1000)
+print(listener.listen(3))
